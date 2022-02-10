153adcafdc0b0d8254a3eae13d15d51dc1ae3bad
Muhammadtalaat <eng.muhamedtalaat@gmail.com>
2022-02-10 12:59:22 +0200

    commit-8:login route

--- controllers/authController.js
+++ controllers/authController.js
@@ -21,7 +21,24 @@ const register =  async (req,res)=>{
     res.status(StatusCodes.CREATED).json({user:tokenUser})
 }
 const login =  async (req,res)=>{
-    res.send('login user');
+    const {email,password} = req.body
+    if(!email || !password)
+    {
+        throw new CustomError.BadRequestError("please provide a valid email and password")
+    }
+    const user = await User.findOne({email})
+    if(!user)
+    {
+        throw new CustomError.UnauthenticatedError("invalid credentails")
+    }
+    const checkPassword = await user.comparePassword(password)    
+    if(!checkPassword)
+    {
+        throw new CustomError.UnauthenticatedError("invalid credentails")
+    }
+    const tokenUser = {name:user.name,userID:user._id,role:user.role}
+    attachCookiesToResponse({res,user:tokenUser})
+    res.status(StatusCodes.CREATED).json({user:tokenUser})
 }
 const logout =  async (req,res)=>{
     res.send('logout user');
